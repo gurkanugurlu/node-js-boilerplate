b0d90fc24bee5bd5dc02d06420cc8253c2f0377f
Gurkan Ugurlu <gurkan.ugurlu@keepnetlabs.com>
2020-10-10 17:11:04 +0300

    new improvements on login

---
+++ src/controller/login.js
@@ -0,0 +1,14 @@
+const User = require('../models/User')
+const login=async ({body:{email,password}}, res) => {
+    try {
+        const user = await User.findByCredentials(email,password)
+        return res.send(user)
+    }
+    catch (err) {
+        return res.status(400).send(err)
+    }
+}
+
+module.exports={
+    login
+}
--- src/schemas/User.js
+++ src/schemas/User.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-module.exports = new mongoose.Schema({
+const bcrypt = require('bcryptjs')
+
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim:true
     },
     age: {
         type: Number,
+        trim:true,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a pozitive number')
@@ -15,6 +19,9 @@ module.exports = new mongoose.Schema({
     },
     email: {
         type: String,
+        required:true,
+        unique:true,
+        trim:true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('It is not email')
@@ -34,3 +41,34 @@ module.exports = new mongoose.Schema({
         }
     }
 })
+
+
+//login operation
+
+UserSchema.statics.findByCredentials=async (email,password) => {
+    const User=this
+    const user=await User.findOne({email})
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch=await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+
+//crpyt operation before every user save if password changes.
+
+UserSchema.pre('save', async function (next) {
+    const user = this
+    
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    
+    next()
+})
+
+module.exports=UserSchema
